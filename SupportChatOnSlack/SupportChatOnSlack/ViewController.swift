8f2d19cd7fb0878c12313d321a5631e004cfdc17
knedle-nikola <nikola@knedle.com>
2016-09-17 22:34:57 +0100

    Dependency configuration for the project

---
+++ SupportChatOnSlack/SupportChatOnSlack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SupportChatOnSlack
+//
+//  Created by Knedle on 17/09/2016.
+//  Copyright © 2016 Knedle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
